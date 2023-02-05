a1e252bf2da68692476166c7c8bdbf139ff18944
K-MHOON <azmria117@gmail.com>
2023-02-05 18:45:54 +0900

    feat: 홈 화면 꾸미기

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
+import '../css/common/Button.scss';
+import '../css/common/Icons.scss';
+import '../css/common/Box.scss';
+import { IoIosCreate, IoIosContacts } from 'react-icons/io';
 
 const Home = () => {
-    return <div>스터디에 온 것을 환영합니다!</div>;
+  return (
+    <div>
+      <div className="middle">스터디에 오신 것을 환영합니다.</div>
+      <div className="middle">
+        <button className="w-btn-outline w-btn-blue-outline">
+          스터디 조회하기 <IoIosContacts className="size25" />
+        </button>
+        <button className="w-btn-outline w-btn-blue-outline">
+          스터디 만들기 <IoIosCreate className="size25" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
