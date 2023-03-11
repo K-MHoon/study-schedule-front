7e46d17d95039f8efcebfff6aaef61559eb64ca4
K-MHOON <azmria117@gmail.com>
2023-03-11 16:31:45 +0900

    feat : api call 예외처리 개선

--- src/containers/member/MemberRegisterContainer.jsx
+++ src/containers/member/MemberRegisterContainer.jsx
@@ -10,13 +10,17 @@ const MemberRegisterContainer = () => {
     try {
       await createMember(memberId, password, name, age);
       alert('회원가입에 성공했습니다!');
-      navigate('/');
+      navigate('/auth/login'); // 로그인 화면으로 이동
     } catch (e) {
       console.log(e);
       if (e.response.status === 504) {
         alert('서버가 점검 중입니다. 관리자에게 문의해주세요.');
       } else {
-        alert('회원가입에 실패했습니다!');
+        if (!e.response.data.errorList) {
+          alert(`[Failed] ${e.response.data.message} \n`);
+        } else {
+          alert(`[Failed] ${e.response.data.errorList[0].message}`);
+        }
       }
     }
   };
--- src/containers/todo/TodoByMemberListContainer.jsx
+++ src/containers/todo/TodoByMemberListContainer.jsx
@@ -33,6 +33,15 @@ const TodoByMemberListContainer = () => {
       navigate(`/member/${memberId}/schedule`);
     } catch (e) {
       console.log(e);
+      if (e.response.status === 504) {
+        alert('서버가 점검 중입니다. 관리자에게 문의해주세요.');
+      } else {
+        if (!e.response.data.errorList) {
+          alert(`[Failed] ${e.response.data.message} \n`);
+        } else {
+          alert(`[Failed] ${e.response.data.errorList[0].message}`);
+        }
+      }
     }
   };
 
