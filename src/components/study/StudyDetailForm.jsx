9d67c7b4a8f6a5a93469f9763b522bf0c0c7b839
K-MHOON <azmria117@gmail.com>
2023-03-27 20:01:35 +0900

    feat : 스터디 상세 조회 폼 구성

--- src/components/study/StudyDetailForm.jsx
+++ src/components/study/StudyDetailForm.jsx
@@ -1,7 +1,93 @@
 import React from 'react';
+import { Col, Container, Form, Row, Spinner } from 'react-bootstrap';
+import styled from 'styled-components';
+import { SubmitButton } from '../common/CustomButton';
 
-const StudyDetailForm = () => {
-  return <div></div>;
+const CleanDisabledForm = styled(Form.Control)`
+  &:disabled {
+    background-color: white;
+  }
+`;
+
+const StudyDetailForm = ({ study, loading = true }) => {
+  return (
+    <>
+      {loading && (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
+      {!loading && study && (
+        <Container>
+          <Row>
+            <Form.Label column lg={2}>
+              스터디 이름
+            </Form.Label>
+            <Col>
+              <CleanDisabledForm type="text" value={study.studyName} disabled />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Form.Label column lg={2}>
+              스터디 방장
+            </Form.Label>
+            <Col>
+              <CleanDisabledForm
+                type="text"
+                value={study.leaderName}
+                disabled
+              />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Form.Label column lg={2}>
+              스터디 내용
+            </Form.Label>
+            <Col>
+              <CleanDisabledForm type="text" value={study.content} disabled />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Form.Label column lg={2}>
+              현재 가입된 인원
+            </Form.Label>
+            <Col>
+              <CleanDisabledForm
+                type="text"
+                value={study.remainCount}
+                disabled
+              />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Form.Label column lg={2}>
+              최대 인원
+            </Form.Label>
+            <Col>
+              <CleanDisabledForm type="text" value={study.fullCount} disabled />
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Form.Label column lg={2}>
+              스터디 생성일
+            </Form.Label>
+            <Col>
+              <CleanDisabledForm type="text" value={study.createdBy} disabled />
+            </Col>
+          </Row>
+          <br />
+          <SubmitButton onClick={(e) => console.log(e)}>
+            가입 신청하기
+          </SubmitButton>
+        </Container>
+      )}
+    </>
+  );
 };
 
 export default StudyDetailForm;
--- src/containers/study/StudyDetailContainer.jsx
+++ src/containers/study/StudyDetailContainer.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import StudyDetailForm from '../../components/study/StudyDetailForm';
+import { studyDetail } from '../../lib/study';
 
 const StudyDetailContainer = () => {
-  return <StudyDetailForm />;
+  const dispatch = useDispatch();
+  const { studyId } = useParams();
+
+  const { study, loading } = useSelector(({ study, loading }) => ({
+    study: study.study,
+    error: study.error,
+    loading: loading['study/STUDY_DETAIL'],
+  }));
+
+  useEffect(() => {
+    dispatch(studyDetail(studyId));
+  }, [dispatch, studyId]);
+
+  return <StudyDetailForm study={study} loading={loading} />;
 };
 
 export default StudyDetailContainer;
