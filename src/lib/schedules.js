278437c9e70a450fa134d0a2b768a17ea4358031
K-MHOON <azmria117@gmail.com>
2023-05-07 16:42:02 +0900

    feat : 오늘 할 스케줄 목록 조회 리듀서 추가

--- src/lib/schedules.js
+++ src/lib/schedules.js
@@ -8,15 +8,27 @@ import { takeLatest } from 'redux-saga/effects';
 const [LIST_SCHEDULES, LIST_SCHEDULES_SUCCESS, LIST_SCHEDULES_FAILURE] =
   createRequestActionTypes('schedules/LIST_SCHEDULES');
 
+const [TODAY_SCHEDULES, TODAY_SCHEDULES_SUCCESS, TODAY_SCHEDULES_FAILURE] =
+  createRequestActionTypes('schedules/TODAY_SCHEDULES');
+
 // 액션 생성 함수에서 우선적인 처리가 가능하며, 리턴되는 값은 payload로 들어가게 된다.
 export const listSchedules = createAction(LIST_SCHEDULES, (studyId) => studyId);
+
+export const todaySchedules = createAction(TODAY_SCHEDULES);
 
 const listSchedulesSaga = createRequestSaga(
   LIST_SCHEDULES,
   api.fetchScheduleMemberList,
 );
+
+const todaySchedulesSaga = createRequestSaga(
+  TODAY_SCHEDULES,
+  api.fetchTodayScheduleList,
+);
+
 export function* schedulesSaga() {
   yield takeLatest(LIST_SCHEDULES, listSchedulesSaga);
+  yield takeLatest(TODAY_SCHEDULES, todaySchedulesSaga);
 }
 
 const initialState = {
@@ -33,6 +45,16 @@ const schedules = handleActions(
       meta,
     }),
     [LIST_SCHEDULES_FAILURE]: (state, { payload: error, meta }) => ({
+      ...state,
+      error,
+      meta,
+    }),
+    [TODAY_SCHEDULES_SUCCESS]: (state, { payload: schedules, meta }) => ({
+      ...state,
+      schedules,
+      meta,
+    }),
+    [TODAY_SCHEDULES_FAILURE]: (state, { payload: error, meta }) => ({
       ...state,
       error,
       meta,
