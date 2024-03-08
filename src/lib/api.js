9439abb555cc0bfbf2acf476b3f1419286e0ef2f
K-MHOON <azmria117@gmail.com>
2024-03-08 20:43:31 +0900

    feat : API에 jwtAxios 적용

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,106 +1,73 @@
-import { getAccessToken } from '../modules/Cookie';
 import client from './client';
+import jwtAxios from './jwtUtil';
 
-const privateHeader = {
-  headers: {
-    Authorization: `Bearer ${getAccessToken()}`,
-  },
-};
-
-export const fetchStudyMember = (userId) => client.get(`/api/member/${userId}`);
+export const fetchStudyMember = (userId) =>
+  jwtAxios.get(`/api/member/${userId}`);
 
 export const fetchStudyMemberProfile = () =>
-  client.get(`/api/member/profile`, privateHeader);
+  jwtAxios.get(`/api/member/profile`);
 
-export const fetchStudyMemberList = () =>
-  client.get(`/api/members`, privateHeader);
+export const fetchStudyMemberList = () => jwtAxios.get(`/api/members`);
 
 export const fetchStudyDetail = ({ studyId, inviteCode }) =>
-  client.get(`/api/study/${studyId}?invite-code=${inviteCode}`, privateHeader);
+  jwtAxios.get(`/api/study/${studyId}?invite-code=${inviteCode}`);
 
 export const fetchMyStudyDetail = (studyId) =>
-  client.get(`/api/study/my/${studyId}`, privateHeader);
+  jwtAxios.get(`/api/study/my/${studyId}`);
 
-export const fetchMyStudy = () => client.get(`/api/study/my`, privateHeader);
+export const fetchMyStudy = () => jwtAxios.get(`/api/study/my`);
 
 export const fetchSecretStudy = (inviteCode) =>
-  client.get(`/api/study/secret?invite-code=${inviteCode}`, privateHeader);
+  jwtAxios.get(`/api/study/secret?invite-code=${inviteCode}`);
 
 export const removeStudyMember = (studyList) =>
-  client.delete(`/api/study`, {
+  jwtAxios.delete(`/api/study`, {
     data: { studyList: studyList },
-    ...privateHeader,
   });
 
 export const updateRegisterState = (studyId, registerId, changeState) =>
-  client.post(
-    `/api/study/my/${studyId}/state/${registerId}`,
-    { state: changeState },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study/my/${studyId}/state/${registerId}`, {
+    state: changeState,
+  });
 
 export const updateStudySecret = (studyId, secret, password) =>
-  client.post(
-    `/api/study/${studyId}/secret`,
-    { secret, password },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study/${studyId}/secret`, { secret, password });
 
 export const updateMyStudy = (studyId, studyName, content, fullCount) =>
-  client.post(
-    `/api/study/my/${studyId}`,
-    { studyName, content, fullCount },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study/my/${studyId}`, { studyName, content, fullCount });
 
 export const updateTodayScheduleTodo = (clearScheduleTodoList) =>
-  client.post(
-    `/api/schedule/today`,
-    { clearScheduleTodoList: clearScheduleTodoList },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/schedule/today`, {
+    clearScheduleTodoList: clearScheduleTodoList,
+  });
 
 export const kickOffStudyMember = (studyId, memberId) =>
-  client.post(
-    `/api/study/my/${studyId}/member/${memberId}/out`,
-    {},
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study/my/${studyId}/member/${memberId}/out`, {});
 
 export const updateMemberProfile = (name, password, age) =>
-  client.post(`/api/member/profile`, { name, password, age }, privateHeader);
+  jwtAxios.post(`/api/member/profile`, { name, password, age });
 
 export const removeScheduleList = (scheduleList) =>
-  client.delete(`/api/schedule`, {
+  jwtAxios.delete(`/api/schedule`, {
     data: { scheduleList: scheduleList },
-    ...privateHeader,
   });
 
 export const removeTodoList = (todoList) =>
-  client.delete(`/api/todo`, {
+  jwtAxios.delete(`/api/todo`, {
     data: { todoList: todoList },
-    ...privateHeader,
   });
 
-export const removeMemberSelf = () =>
-  client.delete(`/api/member`, {
-    ...privateHeader,
-  });
+export const removeMemberSelf = () => jwtAxios.delete(`/api/member`, {});
 
 export const removeStudyCodeList = (studyId, studyCodeList) =>
-  client.delete(`/api/study/${studyId}/code`, {
+  jwtAxios.delete(`/api/study/${studyId}/code`, {
     data: { inviteCodeList: studyCodeList },
-    ...privateHeader,
   });
 
 export const cancelStudyRegisterList = (studyRegisterList) =>
-  client.post(
-    `/api/study/register/cancel`,
-    {
-      studyRegisterList: studyRegisterList,
-    },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study/register/cancel`, {
+    studyRegisterList: studyRegisterList,
+  });
 
 export const createMember = (memberId, password, name, age) =>
   client.post(`/api/register`, { memberId, password, name, age });
@@ -113,42 +80,36 @@ export const createStudy = (
   fullCount,
   isUse,
 ) =>
-  client.post(
-    `/api/study`,
-    {
-      studyName,
-      content,
-      secret,
-      password,
-      fullCount,
-      isUse,
-    },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study`, {
+    studyName,
+    content,
+    secret,
+    password,
+    fullCount,
+    isUse,
+  });
 
 export const createTodo = (title, content) =>
-  client.post(`/api/todo`, { title, content }, privateHeader);
+  jwtAxios.post(`/api/todo`, { title, content });
 
 export const fetchScheduleMemberList = (studyId) =>
-  client.get(`/api/schedule?studyId=${studyId}`, privateHeader);
+  jwtAxios.get(`/api/schedule?studyId=${studyId}`);
 
 export const fetchTodayScheduleList = (scheduleType) =>
-  client.get(`/api/schedule/today?type=${scheduleType}`, privateHeader);
+  jwtAxios.get(`/api/schedule/today?type=${scheduleType}`);
 
-export const fetchTodoByMemberList = () =>
-  client.get(`/api/todo`, privateHeader);
+export const fetchTodoByMemberList = () => jwtAxios.get(`/api/todo`);
 
-export const fetchRegisterList = () =>
-  client.get(`/api/study/my/register`, privateHeader);
+export const fetchRegisterList = () => jwtAxios.get(`/api/study/my/register`);
 
 export const fetchStudyCodes = (studyId) =>
-  client.get(`/api/study/${studyId}/code`, privateHeader);
+  jwtAxios.get(`/api/study/${studyId}/code`);
 
 export const fetchTodoByScheduleList = (scheduleId) =>
-  client.get(`/api/todo/schedule/${scheduleId}`);
+  jwtAxios.get(`/api/todo/schedule/${scheduleId}`);
 
 export const fetchScheduleDetail = (scheduleId) =>
-  client.get(`/api/schedule/${scheduleId}`, privateHeader);
+  jwtAxios.get(`/api/schedule/${scheduleId}`);
 
 export const fetchPublicStudyList = ({ name, leader, page, size, sort }) =>
   client.get(
@@ -156,17 +117,18 @@ export const fetchPublicStudyList = ({ name, leader, page, size, sort }) =>
   );
 
 export const fetchStudyRegister = (studyId, goal, objective, comment) =>
-  client.post(
-    `/api/study/register/${studyId}`,
-    { goal, objective, comment },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/study/register/${studyId}`, { goal, objective, comment });
 
 export const createInviteStudyCode = (studyId) =>
-  client.post(`/api/study/${studyId}/code`, {}, privateHeader);
+  jwtAxios.post(`/api/study/${studyId}/code`, {});
 
-export const login = ({ memberId, password }) =>
-  client.post(`/api/login`, { memberId, password });
+export const login = ({ memberId, password }) => {
+  const formData = new FormData();
+  formData.append('memberId', memberId);
+  formData.append('password', password);
+
+  return client.post(`/api/login`, formData);
+};
 
 export const createSchedule = (
   scheduleId,
@@ -180,21 +142,17 @@ export const createSchedule = (
   studyId,
   scheduleType,
 ) =>
-  client.post(
-    `/api/schedule`,
-    {
-      scheduleId,
-      name,
-      startDate,
-      endDate,
-      isUse,
-      todoList,
-      period,
-      customDay: custom,
-      studyId,
-      scheduleType,
-    },
-    privateHeader,
-  );
+  jwtAxios.post(`/api/schedule`, {
+    scheduleId,
+    name,
+    startDate,
+    endDate,
+    isUse,
+    todoList,
+    period,
+    customDay: custom,
+    studyId,
+    scheduleType,
+  });
 
 export const isAuth = () => client.post('/api/token/check');
